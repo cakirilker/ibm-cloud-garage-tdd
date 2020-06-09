06538fd1469688ab306798fc4cff67d6cef6b20f
İlker Çakır <ilker.cakir@betbull.com>
2020-06-10 01:08:19 +0300

    stack throws underflow error

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -40,7 +40,9 @@ describe('a stack', () => {
     stack.push();
     expect(() => stack.push()).toThrowError('capacity overflow error');
   });
-  it.todo('throw underflow error when popping an empty stack');
+  it('throw underflow error when popping an empty stack', () => {
+    expect(() => stack.pop()).toThrowError('capacity underflow error');
+  });
   it.todo('pops the same one item when pushed');
   it.todo('pops two items with the most recent first');
   it.todo('accepts only a positive capacity');
