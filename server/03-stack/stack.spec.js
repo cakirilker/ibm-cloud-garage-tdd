0a97b6eaa121478887816c16860a3f439c4f97c5
İlker Çakır <ilker.cakir@betbull.com>
2020-06-10 00:40:58 +0300

    stack size starts zero

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -10,7 +10,9 @@ describe('a stack', () => {
   it('starts empty', () => {
     expect(stack.isEmpty()).toBe(true);
   });
-  it.todo('starts with stack size of 0');
+  it('starts with stack size of 0', () => {
+    expect(stack.size()).toBe(0);
+  });
   it.todo('is not empty when pushed');
   it.todo('stack size is 1 when pushed');
   it.todo('stack is empty when pushed and popped');
